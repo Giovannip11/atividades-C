e4736c0fad987c0b9257d6c570b402fc492234fc
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-08-08 09:42:30 -0300

    Create 12.c

---
+++ Atividade Estrutura de dados/12.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+int i=0;
+int n=0;
+int x=0;
+    
+  for(i=0;i<=100;i++){
+     printf("%i\n",i);
+      
+  }
+  while( n<=100){
+      printf("%i\n",n);
+      n++;
+      
+  }
+  do{
+      printf("%i\n",x);
+      x++;
+  }while(x<=100);
+
+return 0;
+}
