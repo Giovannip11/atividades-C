2977733106ebcc2d4e1ece245c85d80b89e0a33a
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-08-29 16:00:56 -0300

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,17 +1,33 @@
-// Fazer o bubbleSort entender o algoritmo;
+#include <stdio.h>
+void imprimir(int *A,int n){
+int i;
+
+printf("Vetor:");
+
+for (i=0;i<n;i++){
+    printf("|%d|",A[i]);
+}
+
+}
+
 void bubbleSort(int *A,int n){
-    int i,maior,j,esq,menor;
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(A[i]<A[j]){
-                maior = A[j];
-                menor=A[i];
-            }
-            if (menor > maior){
-                menor=maior;
-            }
-            
+int i,esq,dir,j;
+for (i=0;i<n-1;i++){
+    for(j=0;j<n-1-i;j++){
+        if(A[j]>A[j+1]){
+        int t = A[i];
+        A[j+1] = t;
         }
-        
     }
+}
+
+}
+
+int main (void){
+
+int A[7]={23,4,67,-8,54,90,21};
+imprimir(A,7);
+bubbleSort(A,7);
+imprimir(A,7);
+return 0;
 }
