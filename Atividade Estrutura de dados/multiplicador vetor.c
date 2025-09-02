7228d469f91d54e4ac1ec03c4e374940ce1c79f8
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-09-02 15:02:52 -0300

    Update multiplicador vetor.c

--- Atividade Estrutura de dados/multiplicador vetor.c
+++ Atividade Estrutura de dados/multiplicador vetor.c
@@ -1,30 +1,27 @@
-
 #include <stdio.h>
-void imprime(int *m , int n){
-    int i;
-    for (i=0;i<n;i++){
-        printf("%d\t",m[i]);
-    }
+void imprime(int *m,int n){
+int i;
+for(i=0;i<n;i++){
+    printf("%d\t",m[i]);
+}
+}
+void multiplicar(int *m,int num,int tamanho){
+int i;
+int x;
+for (i=0;i<tamanho;i++){
+    m[i]=m[i]*num;
+
 }
 
-void multiplicar(int *m,int n){
-    int i;
-    int l;
-    for (i>0;i<n;i++){
-        l=m[i]* n;
-        printf("%d\t",l);
-    }
 }
-int main()
-{
-    int j;
-    int vet[5]={1,2,3,4,5};
-    printf("Digite um número de 1 a 10:");
-    scanf("%d",&j);
-    imprime(vet,5);
-    multiplicar(vet,j);
-    imprime(vet,5);
-	
-	return 0;
+
+int main(void){
+int j;
+int vet[5]={1,2,3,4,5};
+printf("Digite um número de 1 a 10:");
+scanf("%d",&j);
+imprime(vet,5);
+multiplicar(vet,j,5);
+imprime(vet,5);
 
 }
