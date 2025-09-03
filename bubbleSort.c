106d5752f203b714bddbfe01f8effca770c3b254
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-09-03 09:52:55 -0300

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -7,29 +7,29 @@ printf("Vetor:");
 for (i=0;i<n;i++){
     printf("|%d|",A[i]);
 }
-
 }
-
 void bubbleSort(int *A,int n){
-int i,t,j;
-for (i=0;i<n-1;i++){
-    for(j=0;j<n-1-i;j++){
-
-        if(A[j]>A[j+1]){
-        t = A[j];
-        A[j]=A[j+1];
-        A[j+1] = t;
+    int i,troca,aux;
+    do{
+        troca=0;
+        for(i=0;i<n-1;i++){
+            if(A[i]>A[i+1]){
+                aux=A[i];
+                A[i]=A[i+1];
+                A[i+1]=aux;
+                troca=1;
+            }
         }
-    }
+    }while(troca);
+    
 }
 
-}
+int main()
+{
+    int A[7]={23,4,67,-8,54,90,21};
+    imprimir(A,7);
+    bubbleSort(A,7);
+    imprimir(A,7);
 
-int main (void){
-
-int A[7]={23,4,67,-8,54,90,21};
-imprimir(A,7);
-bubbleSort(A,7);
-imprimir(A,7);
-return 0;
+    return 0;
 }
