bd74373c86bd3cf6a2272b361bb194ff842e76b0
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-09-04 17:48:01 -0300

    Update insertSort.c

--- insertSort.c
+++ insertSort.c
@@ -10,25 +10,29 @@ for (i=0;i<n;i++){
 }
 }
 void insertionSort(int *A,int n){
-    int i,troca,aux;
-   while(i<n){
-       for(i=0;i<n;i++){
-       if(A[i+1]>A[i+2]){
-           aux=A[i+2];
-           A[i+2]=A[i];
-           A[i]=aux;
-           
-       }
-   }
- }
+    int i,aux,j;
+    for(i=1;i<n;i++){
+        aux = A[i];
+        j=i-1;
+    }
+    while(j>=0 && A[j]>aux){
+        A[j+1]=A[j];
+        j = j-1;
+    }
+    A[j+1] = aux;
 }
 
 int main(void){
-    
+
     int A[7]={23,4,67,-8,54,90,21};
     imprimir(A,7);
     insertionSort(A,7);
     imprimir(A,7);
+
+
+
+return 0;
+}
 
 
 
