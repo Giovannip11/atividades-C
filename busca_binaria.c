29f25416ff6746bb237bc6965088c50e72b9b0d1
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-09-24 08:16:29 -0300

    Update busca_binaria.c

--- busca_binaria.c
+++ busca_binaria.c
@@ -1,37 +1,37 @@
-
 #include <stdio.h>
 #include <math.h>
-
-int busca_binaria(int n,int *A,int t){
-int posicao;
-int meio;
-int inicio=0;
-int fim =t-1;
-meio= inicio +(fim - inicio)/2;
-
-while(inicio<=fim){
-    meio= inicio +(fim - inicio)/2;
-    if(A[meio]==n){
+int busca_binaria(int *vet,int n,int t){
+    int i=0;
+    int fim= t;
+    int meio;
+    while(i<=fim){
+    meio = i + (fim - i)/2;
+    if(vet[meio]==n) {
         return meio;
+    }   
+    if (vet[meio]<n){
+        i = meio +1;
     }
-    if(A[meio]<n){
-        inicio = meio +1;
-
+    if (vet[meio]>n){
+        fim = meio -1;
     }
-    else
-        fim=meio-1;
+    }
+    return -1;
 }
-return -1;
-}
-int main (void){
-    int A[7]={-8,4,21,23,54,67,90};
-    int x;
-    int resultado;
-    int t=7;
-
-    printf("Digite o numero que deseja encontrar:");
-    scanf("%i",&x);
-    resultado=busca_binaria(x,A,t);
-    printf("%d",resultado);
-
+int main()
+{
+    int t = 7;
+    int n;
+    int vet[7]= {1,2,3,4,5,6,7};
+    printf("Digite o numero que deseja achar:");
+    scanf("%d",&n);
+    int resultado = busca_binaria(vet,n,t);
+    if (resultado != -1){
+        printf("O resultado eh posicao:%d",resultado);
+    }else{
+        printf("Numero n encontrado!");
+    }
+    
+    
+    return 0;
 }
