c5b52a9b1a4d8cdafbb61710ef1cdd5c2ccb9ae8
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-10-22 10:47:03 -0300

    Create pilha.c

---
+++ pilha.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cel{
+    int cont;
+    struct cel *prox;
+}cel;
+
+typedef struct cel* Pilha;
+
+Pilha* criarPilha(){
+    Pilha *pi = (Pilha*)malloc(sizeof(Pilha));
+    if(pi!=NULL){
+        *pi=NULL;
+    }
+    return pi; 
+    
+}
+Pilha push(Pilha topo, int x){
+    cel* n = (cel*)malloc(sizeof(cel));
+    if(n == NULL){return 0;}
+    
+    n->cont = x;
+    n->prox = topo;
+    
+    
+    return n;
+}
+
+Pilha pop (Pilha topo){
+    if(top == NULL){return 0;}
+    
+    
+}
+
+int main(void){
+    
+    
+}
