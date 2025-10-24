e8dff9f00da8c6b2245dd8a212a9878cd1ca228b
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-10-24 15:21:17 -0300

    Update pilha.c

--- pilha.c
+++ pilha.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-// add imprimir, acho q n adicionei...
+
 typedef struct cel{
     int cont;
     struct cel *prox;
@@ -13,27 +13,67 @@ Pilha* criarPilha(){
     if(pi!=NULL){
         *pi=NULL;
     }
-    return pi; 
-    
+    return pi;
+
 }
 Pilha push(Pilha topo, int x){
     cel* n = (cel*)malloc(sizeof(cel));
     if(n == NULL){return 0;}
-    
+
     n->cont = x;
     n->prox = topo;
-    
-    
+
+
     return n;
 }
 
 Pilha pop (Pilha topo){
-    if(top == NULL){return 0;}
-    
-    
+    if(topo == NULL){return 0;}
+
+    cel* temp = topo;
+    topo = topo->prox;
+    free(temp);
+
+
+    return topo;
+
+}
+Pilha showTop (Pilha topo){
+    if(topo==NULL){return 0;}
+
+    printf("%d",topo->cont);
+    return topo;
+}
+Pilha verificarPilha(Pilha topo){
+    if(topo==NULL){
+        printf("Pilha vazia");
+    }
+    return 0;
+}
+void imprimirPilha(Pilha topo){
+    if(topo==NULL){printf("Pilha vazia");}
+    cel* aux = topo;
+    while(aux!=NULL){
+        printf("%d",aux->cont);
+        aux=aux->prox;
+    }
 }
 
+
+
 int main(void){
-    
-    
+    Pilha topo = NULL;
+    topo = push(topo,1);
+    topo = push(topo,2);
+    topo = push(topo,3);
+    topo = push(topo,4);
+    printf("Pilha:");
+    imprimirPilha(topo);
+    topo=pop(topo);
+    printf("\nTopo da pilha:");
+    showTop(topo);
+    printf("\nPilha:");
+    imprimirPilha(topo);
+    printf("\n");
+    return 0;
 }
