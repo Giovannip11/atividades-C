8a6058dce5b9daa367d352c6a1ee4a8a09758a54
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-10-30 16:58:30 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -1,50 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct elemento{
+typedef struct cel{
     int cont;
-    struct elemento *prox;
-}Elem;
+    struct cel *prox;
+}cel;
 
 typedef struct fila{
     int quantidade;
     int max;
-    struct elemento *inicio;
-    struct elemento *fim;
-}Fila
-
+    struct cel *inicio;
+    struct cel *fim;
+}Fila;
 Fila* criaFila(){
-    Fila* fi = (Fila*)malloc(sizeof(Fila));
-    if(fi !=NULL){
+    Fila* fi=(Fila*)malloc(sizeof(Fila));
+    if(fi==NULL){
         fi->quantidade=0;
         fi->max=5;
         fi->inicio=NULL;
-        fi->fim=NULL;
+        fi->fim==NULL;
     }
     return fi;
-    
 }
 
-int enqueue(Fila* fi,int x){
+int enqueue(Fila* fi, int x){
     if(fi==NULL || fi->quantidade>=fi->max){
         return 0;
     }
-    Elem *no = (Elem*)malloc(sizeof(Elem));
-    if(mo = NULL){
+    cel *no=(cel*)malloc(sizeof(cel));
+    if(no==NULL){
         return 0;
     }
-    no->cont = x;
+    no->cont=x;
     no->prox=NULL;
-    
-    fi->fim->prox=no;
-    fi->fim = no;
+    if(fi->inicio==NULL){
+        fi->inicio=no;
+    }else{
+        fi->fim->prox=no;
+    }
+    fi->fim=no;
+    fi->quantidade++;
+    return 1;
+
 }
-fi->quantidade;
-return 1;
+
+int dequeue(Fila* fi){
+    if(fi==NULL || fi->quantidade>=fi->max){
+        return -1;
+    }
+    cel *Noremovido = fi->inicio;
+    int DadoRemovido = Noremovido->cont;
+
+    fi->inicio = Noremovido->prox;
+
+    if (fi->inicio==NULL){
+        fi->fim=NULL;
+    }
+    free(Noremovido);
+
+    fi->quantidade--;
+    return DadoRemovido;
+}
+
+void showFila(Fila* fi){
+    if(fi==NULL || fi->quantidade>=fi->max){
+        printf("Fila vazia...");
+    }
+    cel *temp = fi->inicio;
+    printf("Fila:");
+    while(temp !=NULL){
+        printf("%d",temp->cont);
+
+        temp=temp->prox;
+    }
+    if(temp !=NULL){
+        printf("->");
+    }
+    printf("\n");
+}
+void verificarFilaVazia(Fila* fi){
+    if(fi==NULL || fi->quantidade>=fi->max){
+        printf("Fila vazia");
+    }
+}
+int verificarFilaCheia(Fila* fi){
+    if(fi==NULL){
+        return 0;
+    }
+    if(fi->quantidade >= fi->max){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 int main(void){
-    
-    
-    
-    return 0;
+
+
 }
